35d7b8e9e98eebecbc8bbb5d77a2ec9001fcc579
Dorian Gouilly <139970142+doriangouilly@users.noreply.github.com>
2024-01-07 14:35:09 +0100

    Création automatique du fichier log.txt

--- Raspberry/init.py
+++ Raspberry/init.py
@@ -8,6 +8,10 @@ if __name__ == '__main__':
     sql_path = os.path.join(os.path.dirname(__file__), 'tables.sql')
     if not os.path.isfile(path): #Pas de fichier data.db
 
+        #On crée aussi le fichier log.txt
+        if not os.path.isfile(os.path.join(os.path.dirname(__file__), 'log.txt')):
+            f = open(os.path.join(os.path.dirname(__file__), 'log.txt'),'w')
+            f.close()
         station.log("Création de la base de données...")
         conn = sqlite3.connect(path)
         with open('tables.sql','r') as query:
